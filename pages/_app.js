9c7e2cb82fc80272e8e7eb1607408e0fa1736625
Ghack929309 <jcalixte024@gmail.com>
2023-09-19 20:19:44 -0400

    privacy  page

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import Footer from "../components/footer";
 import PopupWidget from "../components/popupWidget";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<ThemeProvider attribute="class">
-			<Component {...pageProps} />
-			<Footer />
-			<PopupWidget />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+      <Footer />
+      <PopupWidget />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
