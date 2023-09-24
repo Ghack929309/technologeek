d16e5f3aaa2b54a8f90b17f59e147ea69e2d1a8f
Ghack929309 <jcalixte024@gmail.com>
2023-09-23 22:47:28 -0400

    full seo optimisation

--- components/SeoComponent.js
+++ components/SeoComponent.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Script from "next/script";
 
 export const SeoComponent = ({
-  title,
+  title = "TechnoloGeek | Empower Your Unique Vision",
   description = "We're here to turn your digital dreams into reality. Let's talk and bring your ideas to life.",
   image = "/img/logo.svg",
   isHomePage,
@@ -11,38 +11,55 @@ export const SeoComponent = ({
   return (
     <Head>
       {/* DEFAULT */}
+      <title key="title">{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {title != null && (
-        <title key="title">
-          {title + (isHomePage ? "" : " • TechnoloGeek")}
-        </title>
-      )}
-      {description != null && (
-        <meta name="description" key="description" content={description} />
-      )}
-      <meta property="fb:app_id" content="623268441017527" />
+      <meta name="description" key="description" content={description} />
+      <meta
+        name="keywords"
+        content="Web Design, Software Development, Keyword3"
+      />
+
       {/* OPEN GRAPH */}
       <meta property="og:type" key="og:type" content="website" />
       <meta
         property="og:url"
         key="og:url"
-        content={`https://www.technologeek.ca/img/how.png`}
+        content="https://www.technologeek.ca/img/how.png"
       />
-      {title != null && (
-        <meta property="og:title" content={title} key="og:title" />
-      )}
-      {description != null && (
-        <meta
-          property="og:description"
-          key="og:description"
-          content={description}
-        />
-      )}
-
+      <meta property="og:title" content={title} key="og:title" />
+      <meta
+        property="og:description"
+        key="og:description"
+        content={description}
+      />
       <meta
         property="og:image"
         key="og:image"
         content={`https://www.technologeek.ca/img/how.png`}
+      />
+      <meta
+        name="keywords"
+        content="Professional Web Design, Responsive Website Design, Custom Web Development, Creative Web Solutions, User-Friendly Design, Mobile-Optimized Websites, SEO-Friendly Design, E-commerce Web Design, Innovative Web Design, Web Design Services in Canada"
+      />
+
+      <meta
+        name="keywords"
+        content="Software Development Services, Custom Software Solutions, Full-Stack Development, Mobile App Development, Software Engineers, Software Development Company, Agile Software Development, Canada Software Developers, Software Development Experts, Software Development Firm"
+      />
+
+      <meta
+        name="keywords"
+        content="Staff Augmentation Services, IT Staffing Solutions, Skilled IT Professionals, Augmented Staffing, Staffing Agency Canada, IT Talent Pool, Staff Augmentation Experts, Flexible Staffing Solutions, Technology Staffing, On-Demand IT Resources"
+      />
+
+      <meta
+        name="keywords"
+        content="Toronto Web Design, Vancouver Software Development, Staff Augmentation in Montreal, IT Staffing Toronto, Software Developers in Calgary, Canada Tech Talent, Edmonton Web Design, Ottawa IT Staffing, Winnipeg Software Solutions, Vancouver IT Augmentation"
+      />
+
+      <meta
+        name="keywords"
+        content="Healthcare Software Development, E-commerce Web Design, Finance Industry Solutions, Technology Staffing for Startups, Government Software Solutions, Education Web Development, Retail IT Solutions, Manufacturing Software Experts, Non-profit Web Design, Real Estate Software Development"
       />
 
       {/* TWITTER */}
@@ -73,22 +90,6 @@ export const SeoComponent = ({
         key="twitter:image"
         content={`https://www.technologeek.ca/img/how.png`}
       />
-      <meta
-        name="google-site-verification"
-        content="j1duf8XRaKuZyGvhPd8GkYXHG7LI4GYbIvAXBsqTC9U"
-      />
-      {/* <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-K4ZD0K08LV"></script>
-      <script>
-        {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)}
-    gtag('js', new Date());
-  
-    gtag('config', 'G-K4ZD0K08LV');
-    `}
-      </script> */}
     </Head>
   );
 };
