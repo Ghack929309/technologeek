868cb68d81bb40e35a1f76046fc218884db80bf6
paterson1720 <lesage1720@gmail.com>
2023-10-17 00:57:29 -0400

    add blog service pricing card

--- pages/blog-service.js
+++ pages/blog-service.js
@@ -6,6 +6,8 @@ import Section from "../components/section";
 import { LightBulbIcon } from "@heroicons/react/24/solid";
 import Benefit from "../components/benefit";
 import Contact from "../components/contact";
+import { GradientContainer } from "../components/gradientContainer";
+import Link from "next/link";
 
 const BlogServicePage = () => {
   const { t, loading } = useTranslation();
@@ -23,6 +25,27 @@ const BlogServicePage = () => {
           title={t("sections.blogService.title")}
         >
           {t("sections.blogService.subtitle")}
+          <div className="flex justify-center mt-6">
+            <GradientContainer
+              border="2"
+              className=" h-fit rounded-xl max-w-lg text-center text-gray-900  shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]  dark:shadow-black/20 p-8 dark:text-gray-300"
+            >
+              <h3 className="mb-4 text-2xl font-semibold">
+                {t("sections.blogService.pricingTitle")}
+              </h3>
+              <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                {t("sections.blogService.pricingSubtitle")}
+              </p>
+              <div className="flex justify-center items-baseline my-8">
+                <Link
+                  href="#contact-us"
+                  className="px-6 py-2 text-white bg-gradient-to-r from-teal-500 via-teal-500 to-green-500 rounded-md md:ml-5"
+                >
+                  {t("sections.blogService.price")}
+                </Link>
+              </div>
+            </GradientContainer>
+          </div>
         </Section>
 
         <Section id="our-blog-service-offer" title={t("sections.blogService.ourOfferTitle")}>
